c6e2ec4c1930d9681ced12f5f0471b77e55b8faa
unknown <viveksaraswat2710@gmail.com>
2022-10-25 17:31:39 +0530

    first commit

---
+++ Backend/models/courier.models.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    pickupLocation: {
+        type: String,
+        required: true,
+    },
+    pickupAddress: {
+        type: String,
+        required: true,
+    },
+    endLocation: {
+        type: String,
+        required: true,
+    },
+    endAddress: {
+        type: String,
+        required: true,
+    },
+    courierType: {
+        type: String,
+        required: true
+    },
+    courierContent: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default : 'Booked'
+    },
+    customerId : {
+        type : String
+    }
+
+}, {collection : 'couriers'}
+);
+
+var courier = new mongoose.model('Courier', schema);
+module.exports = courier;
